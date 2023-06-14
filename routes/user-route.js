808ab699b1aea3bb6d9ab913477e63cf3bcd5f4d
Lekso4ka <lexysnake@gmail.com>
2023-06-14 15:44:13 +0300

    hotfix

--- routes/user-route.js
+++ routes/user-route.js
@@ -1,8 +1,9 @@
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
 const u = require('../controllers/user-controller.js');
 
 router.get("/users", u.getAllUsers);
-router.get("/users/:id", u.getSingleUsers);
+router.get("/users/:id", u.getSingleUser);
 router.post("/users/auth", u.auth);
 router.post("/users/add", u.addUser);
 router.put("/users/update/:id", u.updUser);
