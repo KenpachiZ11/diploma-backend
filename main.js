01c614b59c4ab9e2a8790fe64a8cf97a08bfadb1
KenpachiZ11 <74604119+KenpachiZ11@users.noreply.github.com>
2023-06-16 22:10:34 +0300

    fix

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({extended : true});
 const mongoose = require('mongoose');
-const cors = require("cors");
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT;
 
