9d305d19922a2a3b3b7af447359358ca605a3043
KenpachiZ11 <74604119+KenpachiZ11@users.noreply.github.com>
2023-06-19 00:22:15 +0300

    fix

--- controllers/order-controller.js
+++ controllers/order-controller.js
@@ -20,41 +20,12 @@ const orderToEmail = (req, res) => {
         }
     }));
 
-    // const mailData = {
-    //     from: 'youremail@gmail.com',  // sender address
-    //     to: req.body.to,   // list of receivers
-    //     subject: 'Sending Email using Node.js',
-    //     text: 'That was easy!',
-    //     html: '<b>Hey there! </b>',
-    // };
-
-    console.log(req.body);
-
-    
-
     let mailOptions = {
         from: 'danek0011@gmail.ru',
-        // to: 
         to: req.body.to,
         subject: req.body.subject,
         text: req.body.text,
-        // html: 
-        //     `
-        //         <div style="padding:10px;border-style: ridge">
-        //         <h3>Details Order</h3>
-        //         <ul>
-        //             <li>Email: ${req.body.to}</li>
-        //             <li>Subject: ${req.body.subject}</li>
-        //             <li>Message: ${req.body.text}</li>
-        //         </ul>
-        //     `
     };
-            
-            // <li>Message: ${req.body.info.title}</li>
-            // <li>Message: ${req.body.info.description}</li>
-            // <li>Message: ${req.body.info.author}</li>
-            // <li>Message: ${req.body.info.linkImage}</li>
-
     console.log(mailOptions)
 
     transporter.sendMail(mailOptions, function(error, info){
