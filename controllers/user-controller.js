e8434662b2b795dbbcd0d25841abc2bb8beec33c
Leksa <lexysnake@gmail.com>
2023-06-18 14:03:33 +0000

    fix aaaaa

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,41 +1,20 @@
-<<<<<<< HEAD
-const User = require("../models/user.js");
-
-const getAllUsers = (req, res) => {
-    User
-        .find({}).select("-password -__v")
-=======
 const User = require('../models/user.js');
 
 const getAllUsers = (req, res) => {
     User
         .find({}).select('-password -__v')
->>>>>>> 1b9f474d82b64b081d06ce03bf1de0e6679f4243
         .then((users) => res.json(users))
         .catch(err => console.error(err))
 }
 const getSingleUser = (req, res) => {
     let id = req.params.id
     User
-<<<<<<< HEAD
-        .findOne({"_id": id}).select("-password -__v")
-=======
         .findOne({'_id': id}).select('-password -__v')
->>>>>>> 1b9f474d82b64b081d06ce03bf1de0e6679f4243
         .then((user) => res.json(user))
         .catch(err => console.error(err))
 }
 const auth = (req, res) => {
     if (!req.body.email && !req.body.login) {
-<<<<<<< HEAD
-        res.json({"message": "Пожалуйста заполните адрес электронной почты или логин"});
-    }
-    if (!req.body.password) {
-        res.json({"message": "Пожалуйста заполните пароль"});
-    }
-    User
-        .findOne({"password": req.body.password, $or: [{ "email": req.body.email }, { "login": req.body.login }]}).select("-password -__v")
-=======
         res.json({'message': 'Пожалуйста заполните адрес электронной почты или логин'});
     }
     if (!req.body.password) {
@@ -43,30 +22,11 @@ const auth = (req, res) => {
     }
     User
         .findOne({'password': req.body.password, $or: [{ 'email': req.body.email }, { 'login': req.body.login }]}).select('-password -__v')
->>>>>>> 1b9f474d82b64b081d06ce03bf1de0e6679f4243
         .then((user) => res.json(user))
         .catch(err => console.error(err))
 }
 const addUser = async (req, res) => {
     if (!req.body) {
-<<<<<<< HEAD
-        res.json({"message": "Данные не отправлены на сервер"});
-    }
-    if (!req.body.email && !req.body.login) {
-        res.json({"message": "Пожалуйста заполните адрес электронной почты или логин"});
-    }
-    if (!req.body.password) {
-        res.json({"message": "Пожалуйста заполните пароль"});
-    }
-    try {
-        let user = await User.findOne({"email": req.body.email})
-        if (user) {
-            res.json({"message": "Такой пользователь уже существует"});
-        } else {
-            new User(req.body).save()
-                .then(user => {
-                    let u = {...user._doc};
-=======
         res.json({'message': 'Данные не отправлены на сервер'});
     }
     if (!req.body.email && !req.body.login) {
@@ -82,8 +42,7 @@ const addUser = async (req, res) => {
         } else {
             new User(req.body).save()
                 .then(user => {
-                    let u = {...user};
->>>>>>> 1b9f474d82b64b081d06ce03bf1de0e6679f4243
+                    let u = {...user._doc};
                     delete u.password;
                     delete u.__v;
                     res.json(u);
@@ -96,17 +55,10 @@ const addUser = async (req, res) => {
 }
 const updUser = async (req, res) => {
     try {
-<<<<<<< HEAD
-        let user = await User.updateOne({"_id": req.params.id}, req.body)
-        if (user?.matchedCount) {
-            User
-                .findOne({"_id": req.params.id}).select("-password -__v")
-=======
         let user = await User.updateOne({'_id': req.params.id}, req.body)
         if (user?.matchedCount) {
             User
                 .findOne({'_id': req.params.id}).select('-password -__v')
->>>>>>> 1b9f474d82b64b081d06ce03bf1de0e6679f4243
                 .then((u) => res.json(u))
                 .catch(err => console.error(err))
         }
@@ -115,13 +67,8 @@ const updUser = async (req, res) => {
     }
 }
 const delUser = (req, res) => {
-<<<<<<< HEAD
-    User.deleteOne({"_id": req.params.id})
-        .then((msg) => res.json({"message": "Пользователь успешно удален"}))
-=======
     User.deleteOne({'_id': req.params.id})
         .then((msg) => res.json({'message': 'Пользователь успешно удален'}))
->>>>>>> 1b9f474d82b64b081d06ce03bf1de0e6679f4243
         .catch(err => console.error(err))
 }
 
